edae2f35335aec09d4df74544212a589fe3825ca
Yury Antonov <yantonov@yandex.ru>
2012-12-19 00:26:10 +0600

    Added recipe 5 for week 1.

---
+++ src/main/java/com/packtpub/j7cc/chapter1/recipe5_sleeping_and_resuming_a_thread/FileMain.java
@@ -0,0 +1,17 @@
+package com.packtpub.j7cc.chapter1.recipe5_sleeping_and_resuming_a_thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class FileMain {
+    public static void main(String[] args) {
+        FileClock clock=new FileClock();
+        Thread thread=new Thread(clock);
+        thread.start();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        };
+        thread.interrupt();
+    }
+}
