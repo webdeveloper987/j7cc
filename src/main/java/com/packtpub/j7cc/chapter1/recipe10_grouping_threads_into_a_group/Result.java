d48909840927fadcc093324effd5990d865d8234
Yury Antonov <yantonov@yandex.ru>
2012-12-19 01:30:12 +0600

    Added recipe 10 for chapter 1.

---
+++ src/main/java/com/packtpub/j7cc/chapter1/recipe10_grouping_threads_into_a_group/Result.java
@@ -0,0 +1,13 @@
+package com.packtpub.j7cc.chapter1.recipe10_grouping_threads_into_a_group;
+
+public class Result {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
