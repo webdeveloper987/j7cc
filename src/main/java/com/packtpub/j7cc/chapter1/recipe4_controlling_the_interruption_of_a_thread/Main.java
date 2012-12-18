2079e42f49c172507ac3da4bbff8bd8e7ed2baa6
Yury Antonov <yantonov@yandex.ru>
2012-12-18 07:44:21 +0600

    Added recipe 4 for chapter 1.

---
+++ src/main/java/com/packtpub/j7cc/chapter1/recipe4_controlling_the_interruption_of_a_thread/Main.java
@@ -0,0 +1,18 @@
+package com.packtpub.j7cc.chapter1.recipe4_controlling_the_interruption_of_a_thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        FileSearch searcher = new FileSearch("/", ".bashrc");
+        Thread thread = new Thread(searcher);
+        thread.start();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+}
