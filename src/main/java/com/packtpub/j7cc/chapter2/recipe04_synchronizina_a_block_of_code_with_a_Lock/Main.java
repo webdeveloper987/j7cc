a78b9a1e3b65dba618ae39520dd5eb504b001a70
Yury Antonov <yantonov@yandex.ru>
2012-12-20 08:35:18 +0600

    Added recipe 4 for chapter 2.

---
+++ src/main/java/com/packtpub/j7cc/chapter2/recipe04_synchronizina_a_block_of_code_with_a_Lock/Main.java
@@ -0,0 +1,20 @@
+package com.packtpub.j7cc.chapter2.recipe04_synchronizina_a_block_of_code_with_a_Lock;
+
+
+public class Main {
+
+    public static final int NUMBER_OF_THREADS = 10;
+
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread thread[] = new Thread[10];
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            thread[i] = new Thread(new Job(printQueue), "Thread " + i);
+        }
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            thread[i].start();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/packtpub/j7cc/chapter2/recipe04_synchronizina_a_block_of_code_with_a_Lock/PrintQueue.java
@@ -0,0 +1,24 @@
+package com.packtpub.j7cc.chapter2.recipe04_synchronizina_a_block_of_code_with_a_Lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintQueue {
+    private final Lock queueLock = new ReentrantLock();
+
+    public void printJob(Object document) {
+        queueLock.lock();
+        try {
+            Long duration = (long) (Math.random() * 10000);
+            System.out.println(Thread.currentThread().getName()
+                    + ":PrintQueue: Printing a Job during "
+                    + (duration / 1000)
+                    + " seconds");
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+}
