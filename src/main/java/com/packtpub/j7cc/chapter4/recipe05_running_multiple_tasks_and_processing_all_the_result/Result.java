f62ae33b8bfc8cd74c06de4dce0f42d873559aa1
Yury Antonov <yantonov@yandex.ru>
2012-12-23 20:15:55 +0600

    Added recipe 5 for chapter 4.

---
+++ src/main/java/com/packtpub/j7cc/chapter4/recipe05_running_multiple_tasks_and_processing_all_the_result/Result.java
@@ -0,0 +1,22 @@
+package com.packtpub.j7cc.chapter4.recipe05_running_multiple_tasks_and_processing_all_the_result;
+
+public class Result {
+    private String name;
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
