10f4d65e390ceef7d5de753533977b9f59d245be
Yury Antonov <yantonov@yandex.ru>
2012-12-25 01:08:08 +0600

    Added recipe 5 for chapter 5.

---
+++ src/main/java/com/packtpub/j7cc/chapter5/recipe05_cancelling_a_task/Main.java
@@ -0,0 +1,22 @@
+package com.packtpub.j7cc.chapter5.recipe05_cancelling_a_task;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayGenerator generator = new ArrayGenerator();
+        int array[] = generator.generateArray(1000);
+        TaskManager manager = new TaskManager();
+        ForkJoinPool pool = new ForkJoinPool();
+        SearchNumberTask task = new SearchNumberTask(array, 0, 1000, 5, manager);
+        pool.execute(task);
+        pool.shutdown();
+        try {
+            pool.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Main: The program has finished\n");
+    }
+}
