83e12e8da407c0140d8b1d276215111ad4b222cb
Yury Antonov <yantonov@yandex.ru>
2012-12-28 01:03:06 +0600

    Added recipe 3 for chapter 6.

---
+++ src/main/java/com/packtpub/j7cc/chapter6/recipe03_using_blocking_thread_safe_lists_ordered_by_priority/Event.java
@@ -0,0 +1,30 @@
+package com.packtpub.j7cc.chapter6.recipe03_using_blocking_thread_safe_lists_ordered_by_priority;
+
+public class Event implements Comparable<Event> {
+    private int thread;
+    private int priority;
+
+    public Event(int thread, int priority) {
+        this.thread = thread;
+        this.priority = priority;
+    }
+
+    public int getThread() {
+        return thread;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public int compareTo(Event e) {
+        if (this.priority > e.getPriority()) {
+            return -1;
+        } else if (this.priority < e.getPriority()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
