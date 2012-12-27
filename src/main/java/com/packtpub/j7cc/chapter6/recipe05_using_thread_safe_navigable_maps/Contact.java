8c7cfcd29f5a51e585edc0bdf0811f8b38555e0e
Yury Antonov <yantonov@yandex.ru>
2012-12-28 01:21:49 +0600

    Added recipe 5 for chapter 6.

---
+++ src/main/java/com/packtpub/j7cc/chapter6/recipe05_using_thread_safe_navigable_maps/Contact.java
@@ -0,0 +1,19 @@
+package com.packtpub.j7cc.chapter6.recipe05_using_thread_safe_navigable_maps;
+
+public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
